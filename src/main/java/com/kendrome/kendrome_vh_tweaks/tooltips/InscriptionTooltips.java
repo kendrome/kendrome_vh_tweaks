7d768cdb1bf0d407d86e3f1fd27202bcfbf0fabb
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-06-15 13:49:48 -0500

    inscriptions

--- src/main/java/com/kendrome/kendrome_vh_tweaks/tooltips/InscriptionTooltips.java
+++ src/main/java/com/kendrome/kendrome_vh_tweaks/tooltips/InscriptionTooltips.java
@@ -2,6 +2,7 @@ package com.kendrome.kendrome_vh_tweaks.tooltips;
 
 import com.kendrome.kendrome_vh_tweaks.Utils;
 import com.kendrome.kendrome_vh_tweaks.config.ClientConfig;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.item.ItemStack;
@@ -10,23 +11,17 @@ import java.util.List;
 
 public class InscriptionTooltips {
     public static void appendTooltips(ItemStack itemStack, List<Component> toolTip) {
-        if (!ClientConfig.INSCRIPTION_RELATIVE_TOOLTIPS_ENABLED.get() || !Utils.shouldShow(ClientConfig.INSCRIPTION_RELATIVE_TOOLTIPS_KEY.get())) {
+        if (!ClientConfig.INSCRIPTION_RELATIVE_TOOLTIPS_ENABLED.get()
+                || !Utils.shouldShow(ClientConfig.INSCRIPTION_RELATIVE_TOOLTIPS_KEY.get())
+                || itemStack.getTag() == null) {
             return;
         }
 
-        if (itemStack == null || itemStack.getTag() == null)
-            return;
-        var data = itemStack.getTag().getCompound("data");
-        var completion = data.getFloat("completion");
-        var instability = data.getFloat("instability");
-        var time = data.getInt("time") / 20;
+        CompoundTag data = itemStack.getTag().getCompound("data");
+        float completion = data.getFloat("completion");
+        float instability = data.getFloat("instability");
+        int time = data.getInt("time") / 20;
 
-
-        var options = ClientConfig.INSCRIPTIONS_RELATIVE_OPTIONS.get();
-
-        for (var opt : options) {
-
-        }
         ClientConfig.INSCRIPTIONS_RELATIVE_OPTIONS.get().forEach(opt -> {
             if (opt.equals(ClientConfig.InscriptionOptions.CompletionPerTime.toString())) {
                 toolTip.add(new TextComponent("§7" + Utils.formatText((completion * 100) / time) + " §fcompletion/time"));
